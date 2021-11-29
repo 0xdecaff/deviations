86b60fe60398053714144c6fa085ae7ffae7ec6a
0xdecaff <0xdecaff@protonmail.com>
2021-11-29 09:37:18 -0700

    Update

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,2 +1,12 @@
-import {} from "../generated/CorruptionsDeviationRegistry/CorruptionsDeviationRegistry"
-import {} from "../generated/schema"
+import { SetValueCall } from "../generated/CorruptionsDeviationRegistry/CorruptionsDeviationRegistry"
+import { DeviationInfo } from "../generated/schema"
+
+export function handleSetValueCall(call: SetValueCall): void {
+  let entity = new DeviationInfo(call.transaction.hash.toHexString());
+  entity.created = call.block.timestamp;
+  entity.index = call.inputs.index;
+  entity.name = call.inputs.name;
+  entity.contractAddress = call.inputs.contractAddress.toHexString();
+  entity.extraData = call.inputs.extraData;
+  entity.save();
+}
